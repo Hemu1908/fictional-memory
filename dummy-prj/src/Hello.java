b158ae7c3b9dca9bac2cf8d48df6c6016ac5f258
Hemangi <hemangimalgaonkar98@hotmail.com>
2021-03-23 18:00:17 +0530

    modified hello class

--- dummy-prj/src/Hello.java
+++ dummy-prj/src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
 	public void method1(){
 		System.out.println("Hello all, learning to create repository in eclipse");
 		System.out.println("Next Line");
+		System.out.println("Some text");
 	}
 }
