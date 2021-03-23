f47e2227ac42de42004eb799799fb5876b9197c1
Hemangi <hemangimalgaonkar98@hotmail.com>
2021-03-23 17:48:32 +0530

    more lines in hello.java

--- dummy-prj/src/Hello.java
+++ dummy-prj/src/Hello.java
@@ -2,5 +2,8 @@
 public class Hello {
 	public void method1(){
 		System.out.println("Hello all, learning to create repository in eclipse");
+		System.out.println("Line 1");
+		System.out.println("Line 2");
+		System.out.println("Line 3");
 	}
 }
